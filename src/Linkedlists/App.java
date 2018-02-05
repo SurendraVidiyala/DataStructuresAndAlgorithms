d0d76a59310c9a11232f1ec0119537e29a049bc7
surendra_vidiyala <vidiyala.surendra@gmail.com>
2018-02-05 17:49:53 -0600

    created LinkedList and performed operations

---
+++ src/Linkedlists/App.java
@@ -0,0 +1,20 @@
+package Linkedlists;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        List<Integer> myLinkedList = new LinkedList<>();
+        
+        myLinkedList.insert(1);
+        myLinkedList.insert(2);
+        myLinkedList.insert(3);
+        myLinkedList.insert(4);
+        
+        myLinkedList.remove(1);
+        
+        
+        myLinkedList.traverseList();
+
+    }
+}
